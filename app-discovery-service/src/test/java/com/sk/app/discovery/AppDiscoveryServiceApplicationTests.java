847bcf92a0f9e5e56ab5d2932ec98c5b65f37edd
skbiswas <sk_biswas@hotmail.com>
2025-06-17 16:58:26 +0530

    Eurek Service first draft

---
+++ app-discovery-service/src/test/java/com/sk/app/discovery/AppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
